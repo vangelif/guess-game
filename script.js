5a7e68f76b07298f7c0533b35b70e72d3420e5f7
Vangelis <ev.fotiades@yahoo.com>
2023-03-29 17:17:42 +0300

    include DOM elements

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// document.querySelector('.message').textContent = 'Yuuuuhuuuu Number!';
+// document.querySelector('.score').textContent = '10!';
+
+// document.querySelector('.guess').value = 3324;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', () => {
+  console.log('hello!');
+});
